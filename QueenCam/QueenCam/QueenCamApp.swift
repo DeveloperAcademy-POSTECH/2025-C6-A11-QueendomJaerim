a40ccb1ca5f7edb587d34bfc92fada08866b824f
0tak2 <0tak2.code@gmail.com>
2025-09-19 19:32:20 +0900

    Feat: Initial Setting

---
+++ QueenCam/QueenCam/QueenCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueenCamApp.swift
+//  QueenCam
+//
+//  Created by 임영택 on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct QueenCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QueenCam/QueenCamTests/QueenCamTests.swift
@@ -0,0 +1,17 @@
+//
+//  QueenCamTests.swift
+//  QueenCamTests
+//
+//  Created by 임영택 on 9/19/25.
+//
+
+import Testing
+@testable import QueenCam
+
+struct QueenCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
