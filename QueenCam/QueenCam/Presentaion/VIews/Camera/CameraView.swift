a6cb6ce193720ba385415d8a1544156364508dd9
승찬 <anstmdcks12@naver.com>
2025-10-05 21:37:44 +0900

    [Feat] camera: 카메라 프리뷰 레이어 구성 및 화면에 표현

--- QueenCam/QueenCam/Presentaion/VIews/Camera/CameraView.swift
+++ QueenCam/QueenCam/Presentaion/VIews/Camera/CameraView.swift
@@ -18,30 +18,39 @@ extension CameraView: View {
       switch viewModel.isPermissionGranted {
       case true:
         Color.black.ignoresSafeArea()
-        
-        Text("카메라 프리뷰 자리")
-          .foregroundStyle(.white)
+
+        CameraPreview(session: viewModel.manager.session)
+
       case false:
         Color.black.ignoresSafeArea()
-        
-        Text("권한이 거부되었습니다.")
-          .foregroundStyle(.white)
+
+        VStack {
+          Text("권한이 거부되었습니다.")
+            .foregroundStyle(.white)
+
+          Button(action: { openSetting() }) {
+            Text("설정으로 이동하기")
+          }
+        }
       }
-      
-      
     }
-    .alert("카메라 접근 권한", isPresented: $viewModel.isShowSettingAlert, actions: {
-      Button(role: .cancel, action: { })
-      
-      Button(action: {  openSetting() }) {
-        Text("설정으로 이동")
+    .alert(
+      "카메라 접근 권한",
+      isPresented: $viewModel.isShowSettingAlert,
+      actions: {
+        Button(role: .cancel, action: {})
+
+        Button(action: { openSetting() }) {
+          Text("설정으로 이동")
+        }
+      },
+      message: {
+        Text("설정에서 카메라 접근 권한을 허용해주세요.")
       }
-    }, message: {
-      Text("설정에서 카메라 접근 권한을 허용해주세요.")
-    })
+    )
     .task {
       await viewModel.checkPermission()
     }
-    
+
   }
 }
--- QueenCam/QueenCam/Presentaion/ViewModels/Camera/CameraViewModel.swift
+++ QueenCam/QueenCam/Presentaion/ViewModels/Camera/CameraViewModel.swift
@@ -1,35 +1,42 @@
-import Foundation
 import AVFoundation
+import Foundation
 
 @Observable
 @MainActor
 final class CameraViewModel {
-  
+  let manager = CameraManager()
+
   var isPermissionGranted = false
   var isShowSettingAlert = false
-  
+
   func checkPermission() async {
     let status = AVCaptureDevice.authorizationStatus(for: .video)
-    
+
     switch status {
     case .notDetermined:
       let granted = await AVCaptureDevice.requestAccess(for: .video)
       if granted {
         isPermissionGranted = true
+        try? await manager.configureSession()
       } else {
         isPermissionGranted = false
         isShowSettingAlert = true
       }
-      
+
     case .restricted, .denied:
       isPermissionGranted = false
       isShowSettingAlert = true
 
     case .authorized:
       isPermissionGranted = true
-      
+      try? await manager.configureSession()
+
     @unknown default:
       isPermissionGranted = false
     }
+  }
+
+  func stopSession() {
+    manager.stop()
   }
 }
