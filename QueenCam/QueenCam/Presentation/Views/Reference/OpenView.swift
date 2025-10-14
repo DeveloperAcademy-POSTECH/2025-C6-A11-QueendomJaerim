b06e063d945c53872b6fc35dfa300cd8af7b6eac
Bora <b0rayun@sookmyung.ac.kr>
2025-10-14 21:42:35 +0900

    [Feat] reference: Open/Close View 추가

---
+++ QueenCam/QueenCam/Presentation/Views/Reference/OpenView.swift
@@ -0,0 +1,18 @@
+//
+//  OpenView.swift
+//  QueenCam
+//
+//  Created by Bora Yun on 10/14/25.
+//
+
+import SwiftUI
+
+struct OpenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OpenView()
+}
---
+++ QueenCam/QueenCam/Presentation/Views/Reference/ReferenceView.swift
@@ -0,0 +1,18 @@
+//
+//  ReferenceView.swift
+//  QueenCam
+//
+//  Created by Bora Yun on 10/14/25.
+//
+
+import SwiftUI
+
+struct ReferenceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReferenceView()
+}
