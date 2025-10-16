96f96f2f5a849302d0ba85f8cbc5219511de3470
Bora <b0rayun@sookmyung.ac.kr>
2025-10-16 12:04:49 +0900

    [Feat] guiding: 툴바 추가

---
+++ QueenCam/QueenCam/Presentation/DesignSystem/Components/ToolBarView.swift
@@ -0,0 +1,57 @@
+//
+//  SwiftUIView.swift
+//  QueenCam
+//
+//  Created by Bora Yun on 10/16/25.
+//
+
+import SwiftUI
+
+struct ToolBarView: View {
+    var onAction: (ToolBarAction) -> Void
+    var body: some View {
+      HStack(spacing: 20){
+        //전체 삭제
+        Button {
+          onAction(.clearAll)
+        } label: {
+          Image(systemName: "xmark")
+            .font(.system(size: 25, weight: .semibold))
+            .foregroundStyle(.black)
+            .frame(width: 60, height: 60)
+            .background(.ultraThinMaterial)
+            .clipShape(Circle())
+            .shadow(color: .black.opacity(0.2), radius: 5, x:0 ,y:2)
+        }
+        
+        HStack(spacing: 28) {
+          // 실행 취소(Undo)
+          Button {
+            onAction(.undo)
+          } label : {
+            Image(systemName: "arrow.uturn.left")
+              .font(.system(size: 26, weight: .semibold))
+              .foregroundStyle(.black)
+          }
+          // 재실행(Redo)
+          Button {
+            onAction(.redo)
+          } label: {
+            Image(systemName: "arrow.uturn.right")
+              .font(.system(size: 26, weight: .semibold))
+              .foregroundStyle(.black)
+          }
+        }.padding(.horizontal, 24)
+          .padding(.vertical, 16)
+          .background(.ultraThinMaterial)
+          .clipShape(Capsule())
+          .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 2)
+      }
+    }
+}
+
+#Preview {
+    ToolBarView { action in
+        print("Tapped: \(action)")
+    }
+}
---
+++ QueenCam/QueenCam/Services/Guiding/Enum/ToolBarAction.swift
@@ -0,0 +1,15 @@
+//
+//  ToolBarAction.swift
+//  QueenCam
+//
+//  Created by Bora Yun on 10/15/25.
+//
+
+import Foundation
+
+enum ToolBarAction {
+  case clearAll // 전체삭제
+  case undo // 실행 취소
+  case redo // 재실행
+}
+
