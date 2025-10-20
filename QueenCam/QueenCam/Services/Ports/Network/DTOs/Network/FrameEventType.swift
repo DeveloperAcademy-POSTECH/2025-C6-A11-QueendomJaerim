36857bd990b2c565888f753e720fbcbd8b433492
0tak2 <0tak2.code@gmail.com>
2025-10-20 22:53:11 +0900

    [Refactor] network: split network events to several files

---
+++ QueenCam/QueenCam/Services/Ports/Network/DTOs/Network/FrameEventType.swift
@@ -0,0 +1,39 @@
+//
+//  FrameEventType.swift
+//  QueenCam
+//
+//  Created by 임영택 on 10/20/25.
+//
+
+import Foundation
+
+enum FrameEventType: Codable, Sendable {
+  case add(FramePayload)
+  case replace(FramePayload)
+  case delete(id: UUID)
+  case deleteAll
+
+  struct FramePayload: Codable, Sendable {
+    let id: UUID
+    let origin: PointPayload
+    let size: SizePayload
+    let color: ColorPayload
+  }
+
+  struct PointPayload: Codable, Sendable {
+    let x: Double
+    let y: Double
+  }
+
+  struct SizePayload: Codable, Sendable {
+    let width: Double
+    let height: Double
+  }
+
+  struct ColorPayload: Codable, Sendable {
+    let red: Double
+    let green: Double
+    let blue: Double
+    let opacity: Double
+  }
+}
---
+++ QueenCam/QueenCam/Services/Ports/Network/DTOs/Network/ReferenceImageEventType.swift
@@ -0,0 +1,13 @@
+//
+//  ReferenceImageEventType.swift
+//  QueenCam
+//
+//  Created by 임영택 on 10/20/25.
+//
+
+import Foundation
+
+enum ReferenceImageEventType: Codable, Sendable {
+  case remove
+  case register(imageData: Data)
+}
