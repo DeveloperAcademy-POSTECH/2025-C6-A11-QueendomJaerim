bc6cc553f842186460f5bb1ad0b3d78c43f2f315
0tak2 <0tak2.code@gmail.com>
2025-10-20 23:11:31 +0900

    [Refactor] network: extract graphic-related mapping logics from `FrameMapper` to `GraphicMapper`

--- QueenCam/QueenCam/Services/Ports/Network/Mappers/FrameMapper.swift
+++ QueenCam/QueenCam/Services/Ports/Network/Mappers/FrameMapper.swift
@@ -6,69 +6,25 @@
 //
 
 import Foundation
-import SwiftUI
 
 struct FrameMapper {
   static func convert(payload: FramePayload) -> Frame {
     Frame(
       id: payload.id,
       rect: CGRect(
-        origin: convert(pointPayload: payload.origin),
-        size: convert(sizePayload: payload.size)
+        origin: GraphicMapper.convert(pointPayload: payload.origin),
+        size: GraphicMapper.convert(sizePayload: payload.size)
       ),
-      color: convert(colorPayload: payload.color)
+      color: GraphicMapper.convert(colorPayload: payload.color)
     )
   }
 
   static func convert(frame: Frame) -> FramePayload {
     FramePayload(
       id: frame.id,
-      origin: convert(cgPoint: frame.rect.origin),
-      size: convert(cgSize: frame.rect.size),
-      color: convert(color: frame.color)
-    )
-  }
-
-  // MARK: - CGPoint <-> PointPayload
-  private static func convert(pointPayload: PointPayload) -> CGPoint {
-    CGPoint(x: pointPayload.x, y: pointPayload.y)
-  }
-
-  private static func convert(cgPoint: CGPoint) -> PointPayload {
-    PointPayload(x: cgPoint.x, y: cgPoint.y)
-  }
-
-  // MARK: - CGSize <-> SizePayload
-  private static func convert(sizePayload: SizePayload) -> CGSize {
-    CGSize(width: sizePayload.width, height: sizePayload.height)
-  }
-
-  private static func convert(cgSize: CGSize) -> SizePayload {
-    SizePayload(width: cgSize.width, height: cgSize.height)
-  }
-
-  // MARK: - Color <-> ColorPayload
-  private static func convert(colorPayload: ColorPayload) -> Color {
-    Color(red: colorPayload.red, green: colorPayload.green, blue: colorPayload.blue, opacity: colorPayload.opacity)
-  }
-
-  private static func convert(color: Color) -> ColorPayload {
-    let uiColor = UIColor(color)
-
-    var red: CGFloat = 0
-    var green: CGFloat = 0
-    var blue: CGFloat = 0
-    var alpha: CGFloat = 0
-
-    guard uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha) else {
-      return .init(red: 0.0, green: 0.0, blue: 0.0, opacity: 1.0)
-    }
-
-    return ColorPayload(
-      red: Double(red),
-      green: Double(green),
-      blue: Double(blue),
-      opacity: Double(alpha)
+      origin: GraphicMapper.convert(cgPoint: frame.rect.origin),
+      size: GraphicMapper.convert(cgSize: frame.rect.size),
+      color: GraphicMapper.convert(color: frame.color)
     )
   }
 }
---
+++ QueenCam/QueenCam/Services/Ports/Network/Mappers/GraphicMapper.swift
@@ -0,0 +1,54 @@
+//
+//  GraphicMapper.swift
+//  QueenCam
+//
+//  Created by 임영택 on 10/20/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct GraphicMapper {
+  // MARK: - CGPoint <-> PointPayload
+  static func convert(pointPayload: PointPayload) -> CGPoint {
+    CGPoint(x: pointPayload.x, y: pointPayload.y)
+  }
+
+  static func convert(cgPoint: CGPoint) -> PointPayload {
+    PointPayload(x: cgPoint.x, y: cgPoint.y)
+  }
+
+  // MARK: - CGSize <-> SizePayload
+  static func convert(sizePayload: SizePayload) -> CGSize {
+    CGSize(width: sizePayload.width, height: sizePayload.height)
+  }
+
+  static func convert(cgSize: CGSize) -> SizePayload {
+    SizePayload(width: cgSize.width, height: cgSize.height)
+  }
+
+  // MARK: - Color <-> ColorPayload
+  static func convert(colorPayload: ColorPayload) -> Color {
+    Color(red: colorPayload.red, green: colorPayload.green, blue: colorPayload.blue, opacity: colorPayload.opacity)
+  }
+
+  static func convert(color: Color) -> ColorPayload {
+    let uiColor = UIColor(color)
+
+    var red: CGFloat = 0
+    var green: CGFloat = 0
+    var blue: CGFloat = 0
+    var alpha: CGFloat = 0
+
+    guard uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha) else {
+      return .init(red: 0.0, green: 0.0, blue: 0.0, opacity: 1.0)
+    }
+
+    return ColorPayload(
+      red: Double(red),
+      green: Double(green),
+      blue: Double(blue),
+      opacity: Double(alpha)
+    )
+  }
+}
