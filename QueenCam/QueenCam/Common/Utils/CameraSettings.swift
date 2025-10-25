a50cb07da05520e8af5ef4f9de00fab8bd339ad4
승찬 <anstmdcks12@naver.com>
2025-10-25 19:10:34 +0900

    [Chore] other: SwiftLint 적용

--- QueenCam/QueenCam/Common/Utils/CameraSettings.swift
+++ QueenCam/QueenCam/Common/Utils/CameraSettings.swift
@@ -12,24 +12,31 @@ final class CameraSettings {
   private let flashKey = "falshMode"
 
   init() {
-    // 기본값 등록
     UserDefaults.standard.register(defaults: [
       livePhotoKey: false,
       gridKey: false,
-      flashKey: FlashMode.off.rawValue
+      flashKey: FlashMode.off.rawValue,
     ])
   }
 
   var livePhotoOn: Bool {
-    get { UserDefaults.standard.bool(forKey: livePhotoKey) }
-    set { UserDefaults.standard.set(newValue, forKey: livePhotoKey) }
+    get {
+      UserDefaults.standard.bool(forKey: livePhotoKey)
+    }
+    set {
+      UserDefaults.standard.set(newValue, forKey: livePhotoKey)
+    }
   }
 
   var gridOn: Bool {
-    get { UserDefaults.standard.bool(forKey: gridKey) }
-    set { UserDefaults.standard.set(newValue, forKey: gridKey) }
+    get {
+      UserDefaults.standard.bool(forKey: gridKey)
+    }
+    set {
+      UserDefaults.standard.set(newValue, forKey: gridKey)
+    }
   }
-  
+
   var flashMode: FlashMode {
     get {
       FlashMode(rawValue: UserDefaults.standard.integer(forKey: flashKey)) ?? .off
