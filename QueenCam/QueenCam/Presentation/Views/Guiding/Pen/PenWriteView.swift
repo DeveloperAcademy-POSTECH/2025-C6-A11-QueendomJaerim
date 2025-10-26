6aea65c9be77f4f25bdf021f8d16715618bf9f19
Bora <b0rayun@sookmyung.ac.kr>
2025-10-26 17:19:17 +0900

    [Feat] disappearpen: 사라지는 펜 1차 구현

--- QueenCam/QueenCam/Presentation/Views/Guiding/Pen/PenWriteView.swift
+++ QueenCam/QueenCam/Presentation/Views/Guiding/Pen/PenWriteView.swift
@@ -3,35 +3,55 @@
 //  QueenCam
 //
 //  Created by Bora Yun on 10/15/25.
-
+import Foundation
 import SwiftUI
 
 /// 펜 가이드라인 작성 뷰 (only 모델)
 struct PenWriteView: View {
   @Bindable var penViewModel: PenViewModel
+  @Binding var isPen: Bool
+  @Binding var isDisappearPen: Bool
   @State private var tempPoints: [CGPoint] = []  // 현재 그리고 있는 선의 좌표(임시)
   private var outerColor = Color.white
   private var innerColor = Color.orange
-  init(penViewModel: PenViewModel) {
+
+  init(penViewModel: PenViewModel, isPen: Binding<Bool>, isDisappearPen: Binding<Bool>) {
     self.penViewModel = penViewModel
+    self._isPen = isPen
+    self._isDisappearPen = isDisappearPen
   }
+
   var body: some View {
     VStack {
       GeometryReader { _ in
         // MARK: - 실제 드로잉 영역
         Canvas { context, _ in
           // 이미 strokes에 저장된 모든 선들 그리기
-          for stroke in penViewModel.strokes {
-            guard stroke.points.count > 1 else { continue }
-            var path = Path()  // Line을 담는 객체
-            path.addLines(stroke.points)
-            context.stroke(path, with: .color(outerColor), style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
-            context.stroke(path, with: .color(innerColor), style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round))
+          if isPen {
+            for stroke in penViewModel.strokes {
+              guard stroke.points.count > 1 else { continue }
+              var path = Path()  // Line을 담는 객체
+              path.addLines(stroke.points)
+              context.stroke(path, with: .color(outerColor), style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
+              context.stroke(path, with: .color(innerColor), style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round))
+            }
+          } else {
+            for stroke in penViewModel.disappearStokes {
+              guard stroke.points.count > 1 else { continue }
+              var path = Path()
+              path.addLines(stroke.points)
+              context.stroke(path, with: .color(outerColor), style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
+              context.stroke(path, with: .color(innerColor), style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round))
+              DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                penViewModel.disappearStokes.removeAll()
+              }
+            }
           }
+
           // 지금 드래그 중인 선들(tempPoints) 그리기: 아직 저장 안된 stroke
           if tempPoints.count > 1 {
             var path = Path()
-            path.addLines(tempPoints)
+              path.addLines(tempPoints)
             context.stroke(path, with: .color(outerColor), style: StrokeStyle(lineWidth: 8, lineCap: .round, lineJoin: .round))
             context.stroke(path, with: .color(innerColor), style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round))
           }
@@ -44,7 +64,11 @@ struct PenWriteView: View {
             }
             .onEnded { _ in
               guard !tempPoints.isEmpty else { return }
-              penViewModel.strokes.append(Pen(points: tempPoints))
+              if isPen {
+                penViewModel.strokes.append(Pen(points: tempPoints))
+              } else {
+                penViewModel.disappearStokes.append(Pen(points: tempPoints))
+              }
               tempPoints.removeAll()
               penViewModel.redoStrokes.removeAll()
             }
