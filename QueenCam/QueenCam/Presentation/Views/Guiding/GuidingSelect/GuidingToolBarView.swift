dd6dc2e1f3ad784bb59ee0b9f9b743fd1e0f5c93
승찬 <anstmdcks12@naver.com>
2025-10-31 00:16:43 +0900

    [Refactor] other: 추가한 컴포넌트로 리팩토링

--- QueenCam/QueenCam/Presentation/Views/Guiding/GuidingSelect/GuidingToolBarView.swift
+++ QueenCam/QueenCam/Presentation/Views/Guiding/GuidingSelect/GuidingToolBarView.swift
@@ -11,42 +11,15 @@ struct GuidingToolBarView: View {
   var onAction: (ToolBarAction) -> Void
   var body: some View {
     HStack(spacing: 20) {
-      //전체 삭제
-      Button {
+      // 전체 삭제
+      PenToolButton(penToolType: .eraser, isActive: true, tapAction: {
         onAction(.clearAll)
-      } label: {
-        Image(systemName: "xmark")
-          .font(.system(size: 25, weight: .semibold))
-          .foregroundStyle(.black)
-          .frame(width: 60, height: 60)
-          .background(.ultraThinMaterial)
-          .clipShape(Circle())
-          .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 2)
-      }
-
-      HStack(spacing: 28) {
-        // 실행 취소(Undo)
-        Button {
-          onAction(.undo)
-        } label: {
-          Image(systemName: "arrow.uturn.left")
-            .font(.system(size: 26, weight: .semibold))
-            .foregroundStyle(.black)
-        }
-        // 재실행(Redo)
-        Button {
-          onAction(.redo)
-        } label: {
-          Image(systemName: "arrow.uturn.right")
-            .font(.system(size: 26, weight: .semibold))
-            .foregroundStyle(.black)
-        }
-      }
-      .padding(.horizontal, 24)
-      .padding(.vertical, 16)
-      .background(.ultraThinMaterial)
-      .clipShape(Capsule())
-      .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 2)
+      })
+      
+      // 실행 취소
+      PenToolButton(penToolType: .undo, isActive: true, tapAction: {
+        onAction(.undo)
+      })
     }
   }
 }
