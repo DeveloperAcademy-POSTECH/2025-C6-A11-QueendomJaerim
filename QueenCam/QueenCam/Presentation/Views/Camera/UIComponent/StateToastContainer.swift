12e01d21bcdf7bf6456c4a2c8fc2299ac173ea0c
0tak2 <0tak2.code@gmail.com>
2025-10-31 16:26:05 +0900

    [Feat] other: StateToast를 가지고 있는 StateToastContainer

---
+++ QueenCam/QueenCam/Presentation/Views/Camera/UIComponent/StateToastContainer.swift
@@ -0,0 +1,22 @@
+//
+//  StateToastContainer.swift
+//  QueenCam
+//
+//  Created by 임영택 on 10/31/25.
+//
+
+import SwiftUI
+
+struct StateToastContainer: View {
+  @State private var stateToastViewModel = StateToastContainerViewModel()
+
+  var body: some View {
+    VStack {
+      if let notification = stateToastViewModel.lastNotificationMessage {
+        StateToast(message: notification.message, isImportant: notification.isImportant)
+      }
+      
+      Spacer()
+    }
+  }
+}
