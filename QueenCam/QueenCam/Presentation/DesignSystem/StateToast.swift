88f6e981cc4a9d9ed77d5fdff0d46cabbbed8302
0tak2 <0tak2.code@gmail.com>
2025-10-31 14:51:30 +0900

    [Feat] other: StateToast 뷰 구현

---
+++ QueenCam/QueenCam/Presentation/DesignSystem/StateToast.swift
@@ -0,0 +1,57 @@
+//
+//  StateToast.swift
+//  QueenCam
+//
+//  Created by 임영택 on 10/31/25.
+//
+
+import SwiftUI
+
+struct StateToast: View {
+  let message: String
+  let isImportant: Bool
+
+  private let importantStateTintColor = Color(red: 254 / 255, green: 188 / 255, blue: 47 / 255)
+
+  var body: some View {
+    Text(message)
+      .typo(.r12)
+      .padding(.vertical, 8)
+      .padding(.horizontal, 16)
+      .glassEffect(isImportant ? .clear.tint(importantStateTintColor) : .clear)
+  }
+}
+
+#Preview {
+  struct StateToastPreviewContainer: View {
+    @State private var isImportant: Bool = true
+    @State private var imageID: UUID = UUID()
+    
+    var body: some View {
+      ZStack {
+        AsyncImage(url: URL(string: "https://picsum.photos/500"))
+          .scaledToFit()
+          .frame(maxWidth: 500, maxHeight: 500)
+          .id(imageID)
+
+        VStack {
+          StateToast(message: "상태 라이팅 영역입니다.", isImportant: isImportant)
+            .onTapGesture {
+              isImportant.toggle()
+            }
+          
+          Spacer()
+          
+          Button("이미지 새로 고침") {
+            imageID = UUID()
+          }
+          .buttonStyle(.glassProminent)
+        }
+        .padding()
+      }
+      .frame(maxWidth: 500, maxHeight: 500)
+    }
+  }
+  
+  return StateToastPreviewContainer()
+}
