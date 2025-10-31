7a8ae0e078f3f85db65cf1d184ce43317628c2aa
승찬 <anstmdcks12@naver.com>
2025-10-31 19:54:24 +0900

    [Chore] other: Boomup버튼 액션 추가

--- QueenCam/QueenCam/Presentation/DesignSystem/BoomupButton.swift
+++ QueenCam/QueenCam/Presentation/DesignSystem/BoomupButton.swift
@@ -1,22 +1,24 @@
 import SwiftUI
 
 struct BoomupButton {
-
+  let tapAction: () -> Void
 }
 
 extension BoomupButton: View {
   var body: some View {
-    Circle()
-      .fill(.gray900)
-      .frame(width: 48, height: 48)
-      .overlay {
-        Image(systemName: "hand.thumbsup")
-          .font(.system(size: 22))
-          .foregroundStyle(.offWhite)
-      }
+    Button(action: { tapAction() }) {
+      Circle()
+        .fill(.gray900)
+        .frame(width: 48, height: 48)
+        .overlay {
+          Image(systemName: "hand.thumbsup")
+            .font(.system(size: 22))
+            .foregroundStyle(.offWhite)
+        }
+    }
   }
 }
 
 #Preview {
-  BoomupButton()
+  BoomupButton(tapAction: { })
 }
