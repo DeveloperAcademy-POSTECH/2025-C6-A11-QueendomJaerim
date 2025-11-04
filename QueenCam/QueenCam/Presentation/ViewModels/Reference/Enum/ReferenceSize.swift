e124dd24bad2a304444b0a7637c694d87fcae3b1
Bora <b0rayun@sookmyung.ac.kr>
2025-11-04 22:59:44 +0900

    [Fix] reference: 네이밍 변경

--- QueenCam/QueenCam/Presentation/ViewModels/Reference/Enum/ReferenceSize.swift
+++ QueenCam/QueenCam/Presentation/ViewModels/Reference/Enum/ReferenceSize.swift
@@ -9,51 +9,51 @@ import Foundation
 
 enum ReferenceSize {
   /// 16:9 비율의 이미지
-  case vertical1
+  case ratio16x9
   /// 4:3 비율의 이미지
-  case vertical2
+  case ratio4x3
   /// 1:1 비율의 이미지
-  case square
+  case ratio1x1
   /// 3:4 비율의 이미지
-  case horizontal1
+  case ratio3x4
   /// 9:16 비율의 이미지
-  case horizontal2
+  case ratio9x16
   /// 타 비율의 이미지
   case none
   
   var width: CGFloat {
     switch self {
-    case .vertical1: return 90
-    case .vertical2: return 105
-    case .square: return 120
-    case .horizontal1: return 140
-    case .horizontal2 : return 160
+    case .ratio16x9: return 90
+    case .ratio4x3: return 105
+    case .ratio1x1: return 120
+    case .ratio3x4: return 140
+    case .ratio9x16: return 160
     case .none: return 105 //일반적인 4:3 비율에 맞춤
     }
   }
   
   var height: CGFloat {
     switch self {
-    case .vertical1: return 160
-    case .vertical2: return 140
-    case .square: return 120
-    case .horizontal1: return 105
-    case .horizontal2 : return 90
+    case .ratio16x9: return 160
+    case .ratio4x3: return 140
+    case .ratio1x1: return 120
+    case .ratio3x4: return 105
+    case .ratio9x16 : return 90
     case .none: return 140 //일반적인 4:3 비율에 맞춤
     }
   }
   static func referenceRatio(width: CGFloat, height: CGFloat) -> ReferenceSize {
     let ratio: CGFloat = height / width
     if ratio == 16/9 {
-      return .vertical1
+      return .ratio16x9
     } else if ratio == 4/3 {
-      return .vertical2
+      return .ratio4x3
     } else if ratio == 1 {
-      return .square
+      return .ratio1x1
     } else if ratio == 3/4 {
-      return .horizontal1
+      return .ratio3x4
     } else if ratio == 9/16 {
-      return .horizontal2
+      return .ratio9x16
     } else {
       return .none
     }
