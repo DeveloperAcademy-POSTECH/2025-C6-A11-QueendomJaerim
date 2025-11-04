4e68f24e930c5a69a2e8a909cda4a933740870bd
Bora <b0rayun@sookmyung.ac.kr>
2025-11-04 22:42:01 +0900

    [Chore] 머지 오류 해결

---
+++ QueenCam/QueenCam/Common/Logger/LogLevel.swift
@@ -0,0 +1,16 @@
+//
+//  LogLevel.swift
+//  QueenCam
+//
+//  Created by 임영택 on 11/3/25.
+//
+
+import Foundation
+
+/// 로그 레벨을 나타내는 enum
+enum LogLevel: String {
+  case info = "INFO"
+  case debug = "DEBUG"
+  case warning = "WARNING"
+  case error = "ERROR"
+}
---
+++ QueenCam/QueenCam/Presentation/Views/Common/LogExporting/LogExportingView.swift
@@ -0,0 +1,41 @@
+//
+//  LogExportingView.swift
+//  QueenCam
+//
+//  Created by 임영택 on 11/2/25.
+//
+
+import SwiftUI
+
+struct LogExportingView: View {
+  @State private var sharingLogFile: SharingLogFile?
+
+  var body: some View {
+    VStack {
+      Text("로그 내보내기")
+        .font(.title)
+
+      if let sharingLogFile {
+        ShareLink(
+          item: sharingLogFile,
+          preview: .init(
+            "로그 내보내기", image: Image(systemName: "text.page"), icon: Image(systemName: "text.page")
+          )
+        )
+      } else {
+        Text("로그 파일을 불러오고 있습니다.")
+          .typo(.m13)
+      }
+    }
+    .onAppear {
+      sharingLogFile = SharingLogFile(
+        url: LogFileAppender.defaultLogFileURL,
+        deviceInfo: .defaultValue
+      )
+    }
+  }
+}
+
+#Preview {
+  LogExportingView()
+}
