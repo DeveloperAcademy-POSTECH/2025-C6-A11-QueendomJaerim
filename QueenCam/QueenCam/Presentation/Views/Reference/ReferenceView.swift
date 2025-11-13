0e3f57ea5f2aa6fc63ef93c1ab04bc075fa181b5
Bora <b0rayun@sookmyung.ac.kr>
2025-11-13 17:31:56 +0900

    [Design] reference: Open=>Close 애니메이션 추가

--- QueenCam/QueenCam/Presentation/Views/Reference/ReferenceView.swift
+++ QueenCam/QueenCam/Presentation/Views/Reference/ReferenceView.swift
@@ -25,6 +25,7 @@ struct ReferenceView: View {
           case .open:  // 레퍼런스 On
             OpenView(referenceViewModel: referenceViewModel, isLarge: $isLarge)
               .offset(referenceViewModel.dragOffset)
+              .transition(.scale)
               .highPriorityGesture(
                 DragGesture(minimumDistance: 5, coordinateSpace: .named(containerName))
                   .onChanged { value in
