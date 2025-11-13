e3b9c696a71a03acbb575791bb9e5d7fdaec40c9
Bora <b0rayun@sookmyung.ac.kr>
2025-11-13 17:33:08 +0900

    [Design] reference: 토스트 내려올때/올라갈때 레퍼런스 이동 애니메이션 추가

--- QueenCam/QueenCam/Presentation/ViewModels/Reference/ReferenceViewModel.swift
+++ QueenCam/QueenCam/Presentation/ViewModels/Reference/ReferenceViewModel.swift
@@ -34,7 +34,7 @@ final class ReferenceViewModel {
 
   // MARK: - Toast State
   private let notificationService: NotificationServiceProtocol
-  
+
   init(
     networkService: NetworkServiceProtocol = DependencyContainer.defaultContainer.networkService,
     notificationService: NotificationServiceProtocol = DependencyContainer.defaultContainer.notificationService
@@ -73,10 +73,8 @@ final class ReferenceViewModel {
   }
 
   func unFold() {
-    withAnimation(.snappy) {
-      state = .open
-      dragOffset = .zero
-    }
+    state = .open
+    dragOffset = .zero
   }
 
   // MARK: - DRAG(for location change)
@@ -125,8 +123,9 @@ extension ReferenceViewModel {
     notificationService.lastNotificationPublisher
       .receive(on: RunLoop.main)
       .sink { [weak self] notification in
-        self?.hasReferenceToast = (notification != nil)
-      }
+        withAnimation(.bouncy(duration: 0.6)){
+          self?.hasReferenceToast = (notification != nil)
+        }}
       .store(in: &cancellables)
   }
 
@@ -139,11 +138,11 @@ extension ReferenceViewModel {
         notificationService.registerNotification(.make(type: .peerRegisterFirstReference))
       }
       if let uiImage = UIImage(data: imageData) {
-        self.image = uiImage        
+        self.image = uiImage
       }
     case .remove:
       self.image = nil
-      notificationService.registerNotification(.make(type:.peerDeleteReference))
+      notificationService.registerNotification(.make(type: .peerDeleteReference))
     }
   }
 }
