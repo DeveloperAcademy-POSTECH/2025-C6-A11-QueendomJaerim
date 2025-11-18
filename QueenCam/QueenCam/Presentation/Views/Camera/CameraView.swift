26d631ab9c04ff4c9962956931145e68543d75f1
0tak2 <0tak2.code@gmail.com>
2025-11-18 11:59:32 +0900

    [Refacot] other: 비율 대응 유틸로 분리

--- QueenCam/QueenCam/Presentation/Views/Camera/CameraView.swift
+++ QueenCam/QueenCam/Presentation/Views/Camera/CameraView.swift
@@ -103,22 +103,6 @@ extension CameraView {
       }
     }
   }
-
-  /// 높이를 너비로 나누었을 때 이 값보다 작으면 뷰를 조정한다. iPad 11 인치 대응.
-  private var shortRatioThreshold: CGFloat {
-    2.0
-  }
-
-  /// 화면 비율이 짧은 비율인지 여부. 짧은 비율이면 뷰를 조정한다. iPad 11 인치 대응.
-  private var isShortScreen: Bool {
-    let scenes = UIApplication.shared.connectedScenes
-    let windowScene = scenes.first as? UIWindowScene
-
-    let screenHeight = windowScene?.screen.bounds.height ?? 0.0
-    let screenWidth = windowScene?.screen.bounds.width ?? 0.0
-
-    return screenHeight / screenWidth < shortRatioThreshold
-  }
 }
 
 extension CameraView: View {
@@ -213,7 +197,7 @@ extension CameraView: View {
           reconnectCancelButtonDidTap: connectionViewModel.reconnectCancelButtonDidTap,
           shutterActionEffect: flashScreen
         )
-        .padding(isShortScreen ? 32 : 0)
+        .padding(DynamicScreenUtils.isShortScreen ? 32 : 0)
 
         CameraBottomContainer(
           currentRole: connectionViewModel.role,
@@ -232,7 +216,7 @@ extension CameraView: View {
           isShowPhotoPicker: $isShowPhotoPicker,
           shutterActionEffect: flashScreen
         )
-        .minimize(isShortScreen)
+        .minimize(DynamicScreenUtils.isShortScreen)
       }
     }
     // MARK: 카메라 세팅 툴
