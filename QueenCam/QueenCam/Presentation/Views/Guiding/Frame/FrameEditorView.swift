04af9cfa825b214209d75ae52f760531720dd01e
Bora <b0rayun@sookmyung.ac.kr>
2025-11-20 15:29:31 +0900

    [Chore] 불필요한 프레임의 Toolbar 주석 삭제

--- QueenCam/QueenCam/Presentation/Views/Guiding/Frame/FrameEditorView.swift
+++ QueenCam/QueenCam/Presentation/Views/Guiding/Frame/FrameEditorView.swift
@@ -35,38 +35,6 @@ struct FrameEditorView: View {
           frameViewModel.selectFrame(nil)
         }
       }
-
-      // MARK: - 프레임의 Toolbar(추가,삭제)
-      //      VStack {
-      //        Spacer()
-      //        HStack(spacing: 28) {
-      //          Button {
-      //            frameViewModel.addFrame(at: CGPoint(x: 0.3, y: 0.4))
-      //          } label: {
-      //            Image(systemName: "plus")
-      //              .font(.system(size: 25, weight: .semibold))
-      //              .foregroundStyle(.black)
-      //              .frame(width: 60, height: 60)
-      //              .background(.ultraThinMaterial)
-      //              .clipShape(Circle())
-      //              .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 2)
-      //          }
-      //          .disabled(frameViewModel.frames.count >= frameViewModel.maxFrames)
-      //
-      //          Button {
-      //            frameViewModel.deleteAll()
-      //          } label: {
-      //            Image(systemName: "xmark")
-      //              .font(.system(size: 25, weight: .semibold))
-      //              .foregroundStyle(.black)
-      //              .frame(width: 60, height: 60)
-      //              .background(.ultraThinMaterial)
-      //              .clipShape(Circle())
-      //              .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 2)
-      //          }
-      //        }
-      //        .padding(.bottom, 30)
-      //      }
     }
   }
 }
