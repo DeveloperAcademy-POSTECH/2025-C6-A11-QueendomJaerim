0711c99d161422d1fa41a9e439894e26d2687dc6
승찬 <anstmdcks12@naver.com>
2025-11-23 15:54:42 +0900

    [Fix] other: 연결 상태시 포토스 버튼 탭시 펜 가이딩 툴 초기화 이슈 수정

--- QueenCam/QueenCam/Presentation/Views/Camera/UIComponent/CameraBottomContainer/CameraBottomContainer+ThumbnailButton.swift
+++ QueenCam/QueenCam/Presentation/Views/Camera/UIComponent/CameraBottomContainer/CameraBottomContainer+ThumbnailButton.swift
@@ -6,10 +6,18 @@ extension CameraView.CameraBottomContainer {
       isShowPhotoPicker.toggle()
       /// nil로만 만드는게 아니라 정보를 줘야함
       /// 그렇게 해야 프레임 같은 경우 상대방도 nil이 됌
-      if activeTool == .frame {
+      switch activeTool {
+      case .frame:
         // 프레임 소유권 변경 및 초기화: 내가 해제하는 경우에만 owner 제거
         frameViewModel.selectedFrameID = nil  // 프레임 선택(isSelected) 초기화 => 제어 모드 종료
         frameViewModel.setFrame(false, nil)  // 상대편 프레임 비활성화 상태 제거(소유자 해제 전파)
+      case .pen:
+        // 펜툴 비활성화 시 세션 strokes들을 persistedStrokes에 저장
+        penViewModel.saveStroke()
+      case .maginPen:
+        break
+      case .none:
+        break
       }
       activeTool = nil
 
