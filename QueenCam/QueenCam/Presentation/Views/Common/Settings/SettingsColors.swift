1403dadc870a3d810dd1b7b9b89856dd4716f663
0tak2 <0tak2.code@gmail.com>
2026-02-14 14:23:45 +0900

    [Feat] settings: 설정페이지 뷰 구성

--- QueenCam/QueenCam/Presentation/Views/Common/Settings/SettingsColors.swift
+++ QueenCam/QueenCam/Presentation/Views/Common/Settings/SettingsColors.swift
@@ -10,4 +10,5 @@ import SwiftUI
 struct SettingsColors {
   static let bannerBackground = Color(red: 0x33 / 255, green: 0x33 / 255, blue: 0x32 / 255)
   static let bannerSubtitle = Color(red: 0xF0 / 255, green: 0xF1 / 255, blue: 0xE8 / 255)
+  static let headerSeparator = Color(red: 0x12 / 255, green: 0x12 / 255, blue: 0x12 / 255)
 }
