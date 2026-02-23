5ce413a3874c298514dde9fa7797df8a6187e426
승찬 <anstmdcks12@naver.com>
2026-02-23 11:51:30 +0900

    [Feat] camera: 카메라 비율 케이스 정의

---
+++ QueenCam/QueenCam/Presentation/ViewModels/Camera/PhotoAspectRatio.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+enum PhotoAspectRatio: String, CaseIterable {
+  case ratio16x9 = "16:9"
+  case ratio1x1 = "1:1"
+  case ratio4x3 = "4:3"
+}
+
+extension PhotoAspectRatio {
+  var value: CGFloat {
+    switch self {
+    case .ratio16x9:
+      return 16.0 / 9.0
+    case .ratio1x1:
+      return 1.0
+    case .ratio4x3:
+      return 4.0 / 3.0
+    }
+  }
+}
