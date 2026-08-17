a251130c5db86166029e4de314a94d8bbe32aec2
0tak2 <0tak2.code@gmail.com>
2026-08-17 23:37:53 +0900

    chore: FAQ 아이템 추가 및 영문 번역

--- QueenCam/QueenCam/Presentation/Views/Common/Settings/FAQ/QuestionAndAnswer.swift
+++ QueenCam/QueenCam/Presentation/Views/Common/Settings/FAQ/QuestionAndAnswer.swift
@@ -16,6 +16,14 @@ struct QuestionAndAnswer: Identifiable {
   static let faqList: [QuestionAndAnswer] = [
     QuestionAndAnswer(
       id: 0,
+      question: "촬영한 사진이 서버에 저장되나요?",
+      answer: """
+        촬영한 사진은 사용자의 기기에만 저장됩니다.
+        앱에서 별도 저장, 보관하지 않으니 안심하고 이용해 주세요!
+        """
+    ),
+    QuestionAndAnswer(
+      id: 1,
       question: "찍자를 사용할 수 있는 기기 사양이 궁금해요.",
       answer: """
         찍자에 적용된 최신 기술을 구동하기 위해서는 아이폰 12 이상의 모델이 필요합니다. 또한, 소프트웨어 버전도 중요해요. 원활한 실행을 위해 iOS 26 이상으로 업데이트가 되어 있어야만 사용하실 수 있습니다.
@@ -29,7 +37,7 @@ struct QuestionAndAnswer: Identifiable {
         """
     ),
     QuestionAndAnswer(
-      id: 1,
+      id: 2,
       question: "기기 검색(페어링)이 잘 안 돼요.",
       answer: """
         우선 연결하려는 기기들의 역할이 다른지 확인해 주세요. 한 명은 모델을, 다른 한 명은 촬영을 선택해야 합니다.
@@ -38,7 +46,7 @@ struct QuestionAndAnswer: Identifiable {
         """
     ),
     QuestionAndAnswer(
-      id: 2,
+      id: 3,
       question: "다른 기기와 연결이 잘 안 안돼요.",
       answer: """
         아래 경로를 통해 ‘Wi-Fi 식별자 재설정’을 진행하면 문제가 해결될 수도 있습니다.
@@ -49,12 +57,12 @@ struct QuestionAndAnswer: Identifiable {
         """
     ),
     QuestionAndAnswer(
-      id: 3,
+      id: 4,
       question: "매번 기기 검색 후, 연결을 해야하는지 궁금해요.",
       answer: "이전에 기기 검색을 통해 페어링하여 등록된 친구 리스트에 있는 기기와는 ‘연결’ 버튼만 눌러 바로 연결할 수 있어요."
     ),
     QuestionAndAnswer(
-      id: 4,
+      id: 5,
       question: "등록된 기기를 지우고 싶어요.",
       answer: """
         아래 경로를 통해 등록된 기기를 삭제할 수 있습니다.
@@ -63,12 +71,12 @@ struct QuestionAndAnswer: Identifiable {
         """
     ),
     QuestionAndAnswer(
-      id: 5,
+      id: 6,
       question: "이전에 연결했던 기기가 등록된 친구에서 보이지 않아요.",
       answer: "iOS 시스템 이슈로 인해 이전에 페어링했던 기기의 정보가 사라지는 문제가 발생할 수 있습니다. 번거로우시겠지만 다시 한번 친구와 기기 등록을 진행해주세요."
     ),
     QuestionAndAnswer(
-      id: 6,
+      id: 7,
       question: "페어링을 진행할 때, 상대 기기에 나온 코드를 입력했지만 그 화면에서 넘어가지 않아요.",
       answer: """
         iOS 시스템 이슈로 인해 페어링이 원할하게 완료되지 않는 문제가 발생할 수 있습니다. 번거로우시겠지만 취소를 눌러 코드를 다시 입력해주세요.
@@ -77,13 +85,13 @@ struct QuestionAndAnswer: Identifiable {
         """
     ),
     QuestionAndAnswer(
-      id: 7,
+      id: 8,
       question: "찍자 사용할 때, 주변 와이파이에 연결하거나 데이터를 켜두어야 하나요?",
       answer:
         "아니요! 찍자는 별도의 네트워크 연결이 필요하지 않습니다. 다만, 두 기기의 빠르고 매끄러운 연결을 위해 Wi-Fi Aware 기술을 사용하고 있기 때문에 와이파이가(와이파이 버튼이) 활성화 되어있기만 하면 됩니다."
     ),
     QuestionAndAnswer(
-      id: 8,
+      id: 9,
       question: "찍자를 사용하다가 발생하는 문제나 오류는 어디로 신고를 하면 되나요?",
       answer: """
         찍자 앱의 설정 페이지에 있는 ‘의견 보내기’를 활용할 수 있습니다.
